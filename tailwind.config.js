4f0ba186b77fe8d3e03d4aed883e11e2b05accea
LucaGambaroPixart <121023235+LucaGambaroPixart@users.noreply.github.com>
2025-01-19 10:59:36 +0100

    removed unused vars

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,11 @@ module.exports = {
       fontFamily:{
         archivioBlack: "Archivo Black"
       },
-      fontSize: {
-        "20rem": "20rem"
-      },
       colors: {
         dark: "black",
         light: "white",
         accent1: "#BCFF2B",
         bg1: "#01161E",
-        bg2: "#17444F",
-        bg3: "#C3FF1F",
       },
       backgroundImage: {
         boxer: "url('/public/images/boxer.png')",
